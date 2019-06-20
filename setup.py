8efae2e186acb920a8e8364128ec98a7da82e783
kevin <kevin@localhost.localdomain>
2019-06-20 18:15:35 +0800

    新增setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+        name='kbot',     # 包名字
+        version='0.1.0',   # 包版本
+        description='This is a tools for operator',   # 简单描述
+        author='zhangjie',  # 作者
+        author_email='zhangjiesoft@163.com',  # 作者邮箱
+        packages=['kbot'],                 # 包
+        install_requires=['fabric==2.4.0','ansible==2.8.0']
+)
